c674854faf65c8988b04c1c4d9d81391f535570a
nickamcd <37389764+nickamcd@users.noreply.github.com>
2018-04-22 12:20:26 -0700

    test 1

--- CECS 343 Project Bullet Hell/Assets/Entities/EnemyFormation/EnemySpawner.cs
+++ CECS 343 Project Bullet Hell/Assets/Entities/EnemyFormation/EnemySpawner.cs
@@ -4,17 +4,33 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour {
     public GameObject enemyPrefab;
+    public float width = 10f;
+    public float height = 5f;
+    private bool movingRight = true;
+    public float speed = 5f;
 
 	// Use this for initialization
 	void Start () {
+        float distanceToCamera = transform.position.z - Camera.main.transform.position.z;
+        Vector3 leftEdge = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distanceToCamera));
+
         foreach (Transform child in transform) {
             GameObject enemy = Instantiate(enemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
             enemy.transform.parent = child;
         }
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+    public void OnDrawGizmos() {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(transform.position, new Vector3(width, height));
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if (movingRight) {
+            transform.position += new Vector3(speed * Time.deltaTime, 0);
+        } else {
+            transform.position += new Vector3(-speed * Time.deltaTime, 0);
+        }
 	}
 }
