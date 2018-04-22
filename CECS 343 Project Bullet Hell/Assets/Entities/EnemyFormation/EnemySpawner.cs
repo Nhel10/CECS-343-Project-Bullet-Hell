63dd0fdc4228688baba2d4983f6873239b8d73ca
nickamcd <37389764+nickamcd@users.noreply.github.com>
2018-04-22 13:15:09 -0700

    Progress 4
    Fixed enemy formation movement

--- CECS 343 Project Bullet Hell/Assets/Entities/EnemyFormation/EnemySpawner.cs
+++ CECS 343 Project Bullet Hell/Assets/Entities/EnemyFormation/EnemySpawner.cs
@@ -4,17 +4,49 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour {
     public GameObject enemyPrefab;
+    public float width = 12f;
+    public float height = 4f;
+    public float speed = 3f;
 
-	// Use this for initialization
-	void Start () {
+    private bool movingRight = true;
+    private float xmax;
+    private float xmin;
+
+    // Use this for initialization
+    void Start () {
+        float distanceToCamera = transform.position.z - Camera.main.transform.position.z;
+        Vector3 leftBoundary = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distanceToCamera));
+        Vector3 rightBoundary = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distanceToCamera));
+
+        xmax = rightBoundary.x;
+        xmin = leftBoundary.x;
+
         foreach (Transform child in transform) {
             GameObject enemy = Instantiate(enemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
             enemy.transform.parent = child;
         }
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+    public void OnDrawGizmos() {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(transform.position, new Vector3(width, height));
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if (movingRight) {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        } else {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+
+        float rightEdgeOfFormation = transform.position.x + (0.5f * width);
+        float leftEdgeOfFormation = transform.position.x - (0.5f * width);
+
+        if (rightEdgeOfFormation > xmax) {
+            movingRight =false;
+        } else if (leftEdgeOfFormation < xmin) {
+            movingRight = true;
+        }
 	}
 }
