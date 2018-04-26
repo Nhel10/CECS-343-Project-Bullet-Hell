512b8801b72ee923f960db28fe3b083073e0a082
Neru <lnyonzon@gmail.com>
2018-04-25 18:04:31 -0700

    Added a glitchy projectile laser

--- CECS 343 Project Bullet Hell/Assets/Scripts/PlayerController.cs
+++ CECS 343 Project Bullet Hell/Assets/Scripts/PlayerController.cs
@@ -5,20 +5,46 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 	public float speed = 5.0f;
+	public float padding = 0.5f;
+	public float projectileSpeed;
+	public float fireRate;
 
-	float xMin = -5;
-	float xMax = 5;
+	public GameObject projectile;
+
+	float xMin;
+	float xMax;
+
+	float yMin;
+	float yMax;
 
 	void Start(){
 		float distance = transform.position.z - Camera.main.transform.position.z;
 		Vector3 leftmost = Camera.main.ViewportToWorldPoint (new Vector3 (0,0,distance));
 		Vector3 rightmost = Camera.main.ViewportToWorldPoint (new Vector3 (1, 0, distance));
-		xMin = leftmost.x;
-		xMax = rightmost.x;
+		xMin = leftmost.x + padding;
+		xMax = rightmost.x - padding;
+
+		Vector3 upMost = Camera.main.ViewportToWorldPoint (new Vector3 (0, 1, distance));
+		Vector3 downMost = Camera.main.ViewportToWorldPoint (new Vector3 (0, 0, distance));
+		yMin = downMost.y + padding;
+		yMax = upMost.y - padding;
+	}
+
+	void Fire(){
+		GameObject beam = Instantiate (projectile, transform.position, Quaternion.identity) as GameObject;
+		beam.GetComponent<Rigidbody2D> ().velocity = new Vector3 (0, projectileSpeed, 0);
 	}
 
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			InvokeRepeating ("Fire", 0.0000001f, fireRate);
+		}
+		if(Input.GetKeyUp(KeyCode.Space)){
+			CancelInvoke ("Fire");
+		}
+
+
 		if (Input.GetKey (KeyCode.LeftArrow)) {
 			//transform.position += new Vector3 (-speed * Time.deltaTime, 0, 0);
 			transform.position += Vector3.left * speed * Time.deltaTime;
@@ -45,7 +71,9 @@ public class PlayerController : MonoBehaviour {
 
 
 		float newX = Mathf.Clamp (transform.position.x, xMin, xMax);
+		float newY = Mathf.Clamp (transform.position.y, yMin, yMax);
 		transform.position = new Vector3 (newX, transform.position.y, transform.position.z);
+		transform.position = new Vector3 (transform.position.x, newY, transform.position.z);
 
 	}
 }
