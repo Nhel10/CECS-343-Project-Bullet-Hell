995b2beb34046368d7383e70f02c49824363cad8
Neru <lnyonzon@gmail.com>
2018-05-06 00:42:52 -0700

    Implemented enemy to laser object collision

---
+++ CECS 343 Project Bullet Hell/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+	public float health;
+
+	public float projectileSpeed;
+	public float fireRate;
+
+	public GameObject projectile;
+	public GameObject powerUpSpawns;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		if (col.gameObject.tag == "PlayerProjectile") {
+			Destroy (col.gameObject);
+		}
+	
+
+	}
+}
