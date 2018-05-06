6fade0b9c097d86cb5cb9eed79738e16404d2779
nickamcd <37389764+nickamcd@users.noreply.github.com>
2018-05-06 12:47:26 -0700

    CollisionAndFiring 1.0
    Have created collision and firing for two seperate enemies. Still need to create more enemies and come up with unique firing patterns for each.

---
+++ CECS 343 Project Bullet Hell/Assets/Scripts/Projectile.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+    public float damage = 100f; //damage caused by projectile
+
+    // function triggers on hit
+    public void Hit()
+    {
+        Destroy(gameObject);    // destroy the projectile that collided with enemy
+    }
+
+    public float getDamage()
+    {
+        return damage;  // return the amount of damage
+    }
+}
