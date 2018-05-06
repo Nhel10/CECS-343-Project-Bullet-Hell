9740fed64ebac6d204913901208cbb4cd277fff5
nickamcd <37389764+nickamcd@users.noreply.github.com>
2018-05-06 15:05:42 -0700

    EnemyCollisionAndFiring 3.0
    Began work on boss enemy

---
+++ CECS 343 Project Bullet Hell/Assets/BossEnemyBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossEnemyBehavior : MonoBehaviour {
+    public Transform[] waypoints;   // waypoints for the boss to travel to
+    public float speed;             // speed at which the boss travels
+    public int curWaypoint;         // current waypoint in the array
+    public bool patrol = true;      // Determines if the boss should be moving or not
+    public Vector3 target;          // target direction
+    public Vector3 moveDirection;   // finds the direction that the boss needs to go to
+    public Vector3 velocity;        // controls the direction in which the boss moves
+	
+	// Update is called once per frame
+	void Update () {
+		if (curWaypoint < waypoints.Length)
+        {
+            target = waypoints[curWaypoint].position;
+            moveDirection = target - transform.position;
+            velocity = GetComponent<RigidBody2D>().velocity;
+
+            if (moveDirection.magnitude < 1)
+            {
+                curWaypoint++;
+            }
+            else
+            {
+                velocity = moveDirection.normalized * speed;
+            }
+        }
+	}
+}
