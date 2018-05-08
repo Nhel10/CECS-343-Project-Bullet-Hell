217cb447c8fbc74cab499e4992d410a80d649a56
nickamcd <37389764+nickamcd@users.noreply.github.com>
2018-05-07 19:31:48 -0700

    BossDesign 2.0
    Boss Shot pattern finished

--- CECS 343 Project Bullet Hell/Assets/Art/Enemy/BossEnemyBehavior.cs
+++ CECS 343 Project Bullet Hell/Assets/Art/Enemy/BossEnemyBehavior.cs
@@ -3,33 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BossEnemyBehavior : MonoBehaviour {
-    public GameObject projectile;           // projectile that the enemy will fire
-    public float enemyMissileSpeed = 10f;   // speed that enemy projectiles travel at
     public float health = 1000f;            // health of enemy object
 
-
-
-    private void Start()
-    {
-        InvokeRepeating("fire", 0.0f, 0.7f);    // repeatedly call fire function
-    }
-
-
-    private void fire()
-    {
-        //create missile slots to fire two enemy projectiles
-        Vector2 missileSlot1 = new Vector2(transform.position.x + .2f, transform.position.y);
-        Vector2 missileSlot2 = new Vector2(transform.position.x - .2f, transform.position.y);
-
-        //fire first enemy projectile
-        GameObject enemyMissile1 = Instantiate(projectile, missileSlot1, Quaternion.identity) as GameObject;
-        enemyMissile1.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -enemyMissileSpeed);
-
-        //fire second enemy projectile
-        GameObject enemyMissile2 = Instantiate(projectile, missileSlot2, Quaternion.identity) as GameObject;
-        enemyMissile2.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -enemyMissileSpeed);
-
-    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
---
+++ CECS 343 Project Bullet Hell/Assets/Art/Enemy/BossShoot.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossShoot : MonoBehaviour {
+
+    public GameObject projectile;       // projectile to be fired
+    public float shotDelay = 0.1f;      // delay between projectiles
+    public float projectileSpeed = 5f;  // speed that the projectile travels at
+
+    private void Start()
+    {
+        Invoke("shoot", shotDelay);
+    }
+
+    // create projectile
+    void shoot()
+    {
+        GameObject bossShot = Instantiate(projectile, transform.position, transform.rotation) as GameObject;
+        bossShot.GetComponent<Rigidbody2D>().velocity = (Vector2)transform.TransformDirection(Vector3.down) * projectileSpeed;
+        Invoke("shoot", shotDelay);
+    }
+}
